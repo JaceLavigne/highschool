1b6128c8f76880a8b88afc7b584d7a0d953a2759
JaceLavigne <72029202+JaceLavigne@users.noreply.github.com>
2020-10-06 08:45:39 -0500

    Create main.cpp

---
+++ CodingChallenge#6/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "end.h"
+#include "dict.h"
+#include "wordDistance.h"
+using namespace std;
+
+int main(){
+  wordDistance("hello", "names");
+  return 0;
+}
